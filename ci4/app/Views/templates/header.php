758dd81320135a47adf4533d45b8b457b4f59075
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-02-27 12:11:11 +0800

    update

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,8 +1,27 @@
 <!doctype html>
 <html>
 <head>
-    <title>CodeIgniter Tutorial</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css">
+    
+    <title>Vieron's Personal Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
-    <h1><?= esc($title) ?></h1>
+    <header>
+      <a href="#" class="logo">vieron</a>
+      <ul>
+        <li><a href="#" class="active">Home</a></li>
+        <li><a href="vagarcia_guests.php">My Guests</a></1i>
+        <li><a href="reg2.php">Register</a></1i>
+      </ul>
+    </header>
