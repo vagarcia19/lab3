753d248a99204fbe3de2d511e50114520a2a3e2d
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-03-02 12:43:28 +0800

    News Section

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class NewsModel extends Model
 {
-    protected $table = 'news';
+    protected $table = 'vagarcia_news';
 
     public function getNews($slug = false)
     {
