6383feb4d04fc94e7c8a3be6fd369a61e8b7b97f
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-03-02 12:46:06 +0800

    News Section

--- ci4/app/Views/news/index.php
+++ ci4/app/Views/news/index.php
@@ -1,23 +1,26 @@
+<div class="par_color">
+    <h2><?= esc($title) ?></h2>
 
-<h2 id = "news_head"><?= esc($title) ?></h2>
+    <?php if (! empty($news) && is_array($news)): ?>
 
-<?php if (! empty($news) && is_array($news)): ?>
+        <?php foreach ($news as $news_item): ?>
 
-    <?php foreach ($news as $news_item): ?>
+            <h3><?= esc($news_item['title']) ?></h3>
 
-        <h3><?= esc($news_item['title']) ?></h3>
+            <div class="main">
+                <?= esc($news_item['body']) ?>
+            </div>
+            <p><a href="/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
 
-        <div class="main">
-            <?= esc($news_item['body']) ?>
-        </div>
-        <p><a href="/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
+        <?php endforeach ?>
 
-    <?php endforeach ?>
+    <?php else: ?>
 
-<?php else: ?>
+        <h3>No News</h3>
 
-    <h3>No News</h3>
+        <p>Unable to find any news for you.</p>
 
-    <p>Unable to find any news for you.</p>
+    <?php endif ?>
 
-<?php endif ?>
+</div>
+    
