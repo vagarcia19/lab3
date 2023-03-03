300db32f3d1d821940c3c3be568dfd4dd25fee35
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-03-03 22:39:45 +0800

    News Items

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -40,7 +40,6 @@ $routes->get('news', [News::class, 'index']);
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
 
-
 /*
  * --------------------------------------------------------------------
  * Additional Routing
