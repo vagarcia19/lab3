4b6812fedbcc184b68bd5f4415a0cc97d98649bd
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-03-05 23:05:57 +0800

    news

--- ci4/app/Views/news/index.php
+++ ci4/app/Views/news/index.php
@@ -1,17 +1,17 @@
 
-    <h2 style="color:#fff"><?= esc($title) ?></h2>
+    <h2 style="color:#fff; padding: 20px; text-align:center"><?= esc($title) ?></h2>
 
     <?php if (! empty($news) && is_array($news)): ?>
 
         <?php foreach ($news as $news_item): ?>
-
+        <div style="color:#fff; padding: 20px; text-align:center">
             <h3 style="color:#fff"><?= esc($news_item['title']) ?></h3>
 
             <div class="main" >
                 <?= esc($news_item['body']) ?>
             </div>
             <p><a href="news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
-
+        </div>
         <?php endforeach ?>
 
     <?php else: ?>
