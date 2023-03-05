2dc3ebb3c91f628c55dba995fe1f32ace98cd1e2
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-03-05 23:01:45 +0800

    guest

--- ci4/app/Views/guest/index.php
+++ ci4/app/Views/guest/index.php
@@ -1,16 +1,17 @@
 
-<h2 style="color:#fff"><?= esc($title) ?></h2><br>
+<h2 style="color:#fff;padding: 20px; text-align:center;font-style:italic;margin-bottom:20px"><?= esc($title) ?></h2><br>
 
 <?php if (! empty($guest) && is_array($guest)): ?>
 
     <?php foreach ($guest as $guest_item): ?>
 
 
-        <div class="main" style="color:#fff padding: 20px; text-align:center">
+        <div class="main" style="color:#fff;padding: 20px; text-align:center">
             <p>Name: <?= esc($guest_item['name']) ?>
             <p>Email: <?= esc($guest_item['email']) ?>
+            <p>Website: <?= esc($guest_item['website']) ?>
+            <p>Gender: <?= esc($guest_item['gender']) ?>
             <p>Comment: <?= esc($guest_item['comment']) ?>
-            <p>Website: <?= esc($guest_item['comment']) ?>
             <p>----------------------</p> <br>
         </div>
 
