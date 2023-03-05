cf2b0feb122429492618871cea6ae7529bcd0297
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-03-05 23:34:12 +0800

    create

--- ci4/app/Views/news/create.php
+++ ci4/app/Views/news/create.php
@@ -6,7 +6,7 @@
     <?= session()->getFlashdata('error') ?>
     <?= validation_list_errors() ?>
 
-    <form action="create" method="post">
+    <form action="news/create" method="post">
         <?= csrf_field() ?>
 
         <label for="title">Title</label>
