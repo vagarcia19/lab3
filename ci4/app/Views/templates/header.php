b614dc0a3807dfc2b8eca9238ac936a795412e4b
vagarcia19 <vagarcia@student.apc.edu.ph>
2023-03-05 23:30:53 +0800

    guest

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -22,8 +22,9 @@
     <a href="home" class="logo">vieron</a>
       <ul>
         <li><a href="home" class="active">Home</a></li>
-        <li><a href="vagarcia_guests">My Guests</a></1i>
+        <li><a href="guest/index">My Guests</a></1i>
         <li><a href="guest">Register</a></1i>
         <li><a href="news">News</a></1i>
+        <li><a href="news/create">Add News</a></1i>
       </ul>
 </header>
